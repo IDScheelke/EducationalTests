12d9e325d8d68d25fc83c6e723b110c461211d40
IDScheelke <IDS@kamstrup.com>
2019-03-25 12:08:53 +0100

    Add semi colon at end of line

--- main.c
+++ main.c
@@ -2,6 +2,6 @@ void main( void )
 {
     printf("First Change\n"); 
     printf("Message in second branch");
-    printf("Inclusion from a second concurrent branch")
+    printf("Inclusion from a second concurrent branch");
     printf("This is from the second branch, checked in after the third");
 }
